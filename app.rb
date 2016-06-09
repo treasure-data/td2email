694ec972b19775e7e562ea34535c5306cf82089b
toru-takahashi <torutakahashi.ayashi@gmail.com>
2016-06-09 23:42:31 +0900

    Support notification by params

--- app.rb
+++ app.rb
@@ -3,11 +3,29 @@ require 'json'
 require 'sendgrid-ruby'
 require 'redcarpet'
 require 'redcarpet/render_strip'
+require 'erb'
+require 'tilt'
+
+put '/' do
+  email = params[:email]
+  template = params[:template]
+  begin
+    payload = JSON.parse(request.body.read)
+    payload = Hash[payload['column_names'].zip(payload['data'].transpose)]
+    template_markdown = create_template(template, payload)
+    if email && template_markdown
+      send_email(email, template_markdown)
+    end
+  end
+  rescue => e
+    STDERR.puts e.backtrace
+  end
+end
 
 put '/:template' do
   template = params[:template]
   begin
-    payload = JSON.parse(request.body.read)  
+    payload = JSON.parse(request.body.read)
     # payload is formatted like
     # { "column_names": ["foo", "bar"],
     #   "data": [[1,2,3], ['a', 'b', 'c']],
@@ -30,15 +48,25 @@ MD2TEXT = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
 
 helpers do
 
+def create_template(template, payload)
+  template_path = "email_templates/#{template}.markdown"
+  template_markdown = nil
+  if File.exist?(template_path)
+    template_markdown = Tilt::StringTemplate.new(template_path)
+                        .render(Object.new, payload)
+    puts template_markdown
+  end
+  return template_markdown
+end
 
 def send_emails(emails, template)
   template_path = "email_templates/#{template}.markdown"
-  
+
   if File.exist?(template_path)
     template_markdown = File.new(template_path).read
     emails.each do |email|
       send_email(email, template_markdown)
-    end 
+    end
   end
 end
 
